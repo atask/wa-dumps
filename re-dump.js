67135c838f6894734ee8ca3afcc860ff32e5fc0f
ᴉuᴉɥɔsɐʇ uɐllɐ <allan.taschini@gmail.com>
2016-06-28 08:49:43 +0200

    Add snapshot save

--- re-dump.js
+++ re-dump.js
@@ -2,6 +2,8 @@
 
 const waSnap = require('wa-snap')
 const swearing = require('swearing')
+const PromiseProxyNode = require('promiseproxy-node')
+const fs = PromiseProxyNode('fs')
 const sources = require('./wa-sources.json')
 
 console.log('Re-dumping databases:')
@@ -12,12 +14,14 @@ swearing.each(sources, source => {
       waPath: source.wa,
       msgstorePath: source.msgstore
     })
-    .then(snapshot => {
-      // TODO: save snapshot
+    .then(snapshot => JSON.stringify(snapshot, null, 2))
+    .then(json => fs.writeFile(source.out, json))
+    .then(() => {
+      console.log(`\t${source.day} -> ${source.out}`)
     })
 }).then(() => {
   console.log('Done.')
 }).catch(err => {
-  console.error(`Error: ${err}`)
+  console.error(`ERROR: ${err.message}`)
   process.exitCode = 1
 })
